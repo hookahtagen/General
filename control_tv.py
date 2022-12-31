2c98c9c39d38925a375abbbbc72a9e333f0bcaa3
hookahtagen <hendrik.siemens@my.com>
2022-12-31 15:45:38 +0100

    Changes: 	new file:   control_tv.py 	new file:   get_ircc.sh 	new file:   send_ircc.sh 	modified:   set_volume.py

---
+++ control_tv.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import requests
+
+ircc_codes = {
+    'Power': 'AAAAAQAAAAEAAAAVAw==',
+    'Input': 'AAAAAQAAAAEAAAAlAw==',
+    'SyncMenu': 'AAAAAgAAABoAAABYAw==',
+    'Hdmi1': 'AAAAAgAAABoAAABaAw==',
+    'Hdmi2': 'AAAAAgAAABoAAABbAw==',
+    'Hdmi3': 'AAAAAgAAABoAAABcAw==',
+    'Hdmi4': 'AAAAAgAAABoAAABdAw==',
+    'Num1': 'AAAAAQAAAAEAAAAAAw==',
+    'Num2': 'AAAAAQAAAAEAAAABAw==',
+    'Num3': 'AAAAAQAAAAEAAAACAw==',
+    'Num4': 'AAAAAQAAAAEAAAADAw==',
+    'Num5': 'AAAAAQAAAAEAAAAEAw==',
+    'Num6': 'AAAAAQAAAAEAAAAFAw==',
+    'Num7': 'AAAAAQAAAAEAAAAGAw==',
+    'Num8': 'AAAAAQAAAAEAAAAHAw==',
+    'Num9': 'AAAAAQAAAAEAAAAIAw==',
+    'Num0': 'AAAAAQAAAAEAAAAJAw==',
+    'Dot': 'AAAAAgAAAJcAAAAdAw==',
+    'CC': 'AAAAAgAAAJcAAAAoAw==',
+    'Red': 'AAAAAgAAAJcAAAAlAw==',
+    'Green': 'AAAAAgAAAJcAAAAmAw==',
+    'Yellow': 'AAAAAgAAAJcAAAAnAw==',
+    'Blue': 'AAAAAgAAAJcAAAAkAw==',
+    'Up': 'AAAAAQAAAAEAAAB0Aw==',
+    'Down': 'AAAAAQAAAAEAAAB1Aw==',
+    'Right': 'AAAAAQAAAAEAAAAzAw==',
+    'Left': 'AAAAAQAAAAEAAAA0Aw==',
+    'Confirm': 'AAAAAQAAAAEAAABlAw==',
+    'Help': 'AAAAAgAAAMQAAABNAw==',
+    'Display': 'AAAAAQAAAAEAAAA6Aw==',
+    'Options': 'AAAAAgAAAJcAAAA2Aw==',
+    'Back': 'AAAAAgAAAJcAAAAjAw==',
+    'Home': 'AAAAAQAAAAEAAABgAw==',
+    'VolumeUp': 'AAAAAQAAAAEAAAASAw==',
+    'VolumeDown': 'AAAAAQAAAAEAAAATAw==',
+    'Mute': 'AAAAAQAAAAEAAAAUAw==',
+    'Audio': 'AAAAAQAAAAEAAAAXAw==',
+    'ChannelUp': 'AAAAAQAAAAEAAAAQAw==',
+    'ChannelDown': 'AAAAAQAAAAEAAAARAw==',
+    'Play': 'AAAAAgAAAJcAAAAaAw==',             # Play
+    'Pause': 'AAAAAgAAAJcAAAAZAw==',            # Play/Pause    
+    'Stop': 'AAAAAgAAAJcAAAAYAw==',
+    'FlashPlus': 'AAAAAgAAAJcAAAB4Aw==',
+    'FlashMinus': 'AAAAAgAAAJcAAAB5Aw==',
+    'Prev': 'AAAAAgAAAJcAAAA8Aw==',
+    'Next': 'AAAAAgAAAJcAAAA9Aw=='
+}
+
+if __name__ == '__main__':
+
+  import sys
+
+  if len( sys.argv ) < 2 or len( sys.argv ) > 2 or sys.argv[ 1 ] not in ircc_codes.keys():
+    print ("Usage: %s <TV_IP> <IRCC_COMMAND>" % sys.argv[ 0 ] )
+    sys.exit( 1 )
+
+  tv_ip = '192.168.2.95'
+  code = ircc_codes[ sys.argv[ 1 ] ]
+
+  cmd = "<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>" + code + "</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope>"
+
+  headers = {
+    'Content-Type': 'text/xml; charset=UTF-8',
+    'SOAPACTION': '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"',
+    'X-Auth-PSK': '1234'
+  }
+
+  r = requests.post( 'http://%s/sony/IRCC' % tv_ip, data=cmd, headers=headers )
+
+  if r.status_code == 200:
+    print ( '✓' )
+  else:
+    print ( 'Command failed (HTTP_CODE: %d, try running it in a console)' % r.status_code )
+    sys.exit( 1 )
--- set_volume.py
+++ set_volume.py
@@ -1,5 +1,37 @@
 import requests
 
+def play_pause(ip, psk):
+    # Set up the IRCC code for the play/pause button
+    ircc_code = 'AAAAAgAAAJcAAAAaAw=='
+    
+    url = "http://192.168.2.95/sony/IRCC"
+    headers = {
+        "HOST": "192.168.2.95",
+        "Accept": "*/*",
+        "Content-Type": "application/xml; charset=UTF-8",
+        "SOAPACTION": "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC",
+        "X-Auth-PSK": "1234",
+        "Connection": "Keep-Alive",
+        "Content-Length": "313",
+    }
+
+    body = """
+    <s:Envelope
+        xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+        s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <s:Body>
+            <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+                <IRCCCode>AAAAAQAAAAEAAAAUAw==</IRCCCode>
+            </u:X_SendIRCC>
+        </s:Body>
+    </s:Envelope>
+    """
+
+    response = requests.post(url, headers=headers, data=body)
+    
+    # Check if the operation was successful
+    return response.status_code == 200
+
 def mute(ip, psk):
 # Send a request to get the current audio mute status
     url = f'http://{ip}/sony/audio'
@@ -70,6 +102,8 @@ def main(argv):
         decrease_volume(ip, psk)
     elif argv[1] == 'mute':
         mute(ip, psk)
+    elif argv[1] == 'play_pause':
+        play_pause(ip, psk)
     else:
         print('Invalid argument. Please specify "increase" or "decrease".')
 
