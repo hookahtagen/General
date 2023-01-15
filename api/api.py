f530f52422774fc9b2f002182a89f8922cd43161
hookahtagen <hendrik.siemens@my.com>
2023-01-15 17:41:54 +0100

    .

--- api/api.py
+++ api/api.py
@@ -6,16 +6,8 @@ from subprocess import Popen, PIPE
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Read menu file to display
-    # This method of creating was chosen to keep the code
-    # clean an simple
-    menu = [ ]
-    with open('/home/hendrik/Documents/General/api//menu.txt', 'r') as f:
-        for line in f:
-            menu.append(line)
-            
-    return Response(menu, mimetype='text/plain')
+def start():
+   return index( )
 
 @app.route('/status')
 def status():
@@ -26,7 +18,7 @@ def status():
     return jsonify(data)
 
 @app.route('/index')
-def test_page():
+def index():
     return render_template('index.html')
 
 @app.errorhandler(404)
@@ -34,4 +26,4 @@ def page_not_found(e):
     return jsonify({'error': 'page not found! Please try again.'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "192.168.2.43", debug=True)
