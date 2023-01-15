a27fd440834f86c4b3133c4c7b2c69ee22de1543
hookahtagen <hendrik.siemens@my.com>
2023-01-15 16:09:33 +0100

    .

---
+++ api/api.py
@@ -0,0 +1,37 @@
+
+from operator import concat
+from flask import Flask, jsonify, Response, render_template
+from subprocess import Popen, PIPE
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Read menu file to display
+    # This method of creating was chosen to keep the code
+    # clean an simple
+    menu = [ ]
+    with open('/home/hendrik/Documents/General/api//menu.txt', 'r') as f:
+        for line in f:
+            menu.append(line)
+            
+    return Response(menu, mimetype='text/plain')
+
+@app.route('/status')
+def status():
+    data = {
+        'status': 'running',
+        'version': '1.0.0'
+        }
+    return jsonify(data)
+
+@app.route('/test_page')
+def test_page():
+    return render_template('test_page.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error': 'page not found! Please try again.'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
